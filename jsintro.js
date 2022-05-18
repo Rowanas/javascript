a1f68dc302f35fda569a6d2f6ff18b6d3b6addcd
Rowanas <rowanatwork@hotmail.co.uk>
2022-05-18 14:11:39 +0100

    fresh commit

---
+++ jsintro.js
@@ -0,0 +1,42 @@
+let forename = "Rowan";
+let surname = "Baker";
+const whereFrom = "Edinburgh";
+const starSign = "Scorpio";
+
+const currentCar = "Seat Leon";
+let currentBoot = "full";
+
+
+console.log(forename);
+console.log(surname);
+console.log(whereFrom);
+console.log(starSign);
+console.log(`My car is a ${currentCar} and my boot is ${currentBoot}`);
+console.log("My car is %c a "+currentCar,"color: yellow");
+console.log(`%c My car is a ${currentCar} and my boot is ${currentBoot}`,`color:green`);
+
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+
+typeof(a);
+typeof(b);
+typeof(c);
+typeof(d);
+typeof(e);
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay;
+
+console.log(`The total bill is ${totalMoney}. the remaining amount of money to be paid is ${totalMoney-moneyPaidSoFar}.`);
+
+console.log(forename)
+console.log(surname)
+console.log(whereFrom)
+console.log(starSign)
+console.log(`My car is a ${currentCar} and my boot is ${currentBoot}`)
+console.log("My car is %c a "+currentCar,"color: yellow")
+console.log(`%c My car is a ${currentCar} and my boot is ${currentBoot}`,`color:green`)
